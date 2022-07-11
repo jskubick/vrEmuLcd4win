69abd861c61987cbc4a7499d1ba04e60565e33b2
Jeff Skubick <bitbang3r@gmail.com>
2022-07-11 16:19:00 -0400

    Works (renders LCD to asterisks & spaces in console)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #define VR_LCD_EMU_STATIC 1
 #include "VrEmuLcd.h"
 
+/*
+ * CP437 uses codepoints 0xDB=top+bottom, DC=bottom, DF=top
+ * so does CP850, 852, 855, 866,
+ * UTF8's Block Elements include U+2580 (upper-half ▀), U+2584 (lower-half ▄), and U+2588 (full-block █)
+ */
+
 int main() {
     VrEmuLcd *lcd = vrEmuLcdNew(20, 4, EmuLcdRomA00);
 
@@ -23,7 +29,6 @@ int main() {
 
     // then periodically, render it.
     vrEmuLcdUpdatePixels(lcd);   // generates a snapshot of the pixels state
-
 
     for (int y = 0; y < vrEmuLcdNumPixelsY(lcd); ++y) {
         for (int x = 0; x < vrEmuLcdNumPixelsX(lcd); ++x) {
