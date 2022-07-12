dd712eebcec391c5cac5bc1f2ae41324f4633fbb
Jeff Skubick <bitbang3r@gmail.com>
2022-07-12 00:25:08 -0400

    Updated after adding SFML

--- main.cpp
+++ main.cpp
@@ -3,14 +3,28 @@
 #include "VrEmuLcd.h"
 #include <SFML/Graphics.hpp>
 
+#define LCD_ROWS 4
+#define LCD_COLS 20
+#define LCD_PIXEL_DIMENSIONS 10
+
 /*
  * CP437 uses codepoints 0xDB=top+bottom, DC=bottom, DF=top
  * so does CP850, 852, 855, 866,
  * UTF8's Block Elements include U+2580 (upper-half ▀), U+2584 (lower-half ▄), and U+2588 (full-block █)
  */
 
+void setPixel(sf::Image& bitmap, int coordX, int coordY, sf::Color color) {
+    int startY = coordY * LCD_PIXEL_DIMENSIONS;
+    int startX = coordX * LCD_PIXEL_DIMENSIONS;
+    for (int y = startY; y < (startY + LCD_PIXEL_DIMENSIONS); y++) {
+        for (int x = startX; x < (startX + LCD_PIXEL_DIMENSIONS); x++) {
+            bitmap.setPixel(x, y, color);
+        }
+    }
+}
+
 int main() {
-    VrEmuLcd *lcd = vrEmuLcdNew(20, 4, EmuLcdRomA00);
+    VrEmuLcd *lcd = vrEmuLcdNew(LCD_COLS, LCD_ROWS, EmuLcdRomA00);
 
     // send it commands:
     vrEmuLcdSendCommand(lcd, LCD_CMD_FUNCTION | LCD_CMD_FUNCTION_LCD_2LINE | 0x10);
@@ -31,26 +45,38 @@ int main() {
     // then periodically, render it.
     vrEmuLcdUpdatePixels(lcd);   // generates a snapshot of the pixels state
 
+
+    sf::Color darkPixel(20,20,20);
+    sf::Color clearPixel(223,246,18);
+    sf::Color noPixel(225,250,20);
+
+    unsigned int windowWidth = (int)(LCD_COLS * 5 * 12);
+    unsigned int windowHeight = (int)(LCD_ROWS * 8 * 12);
+    auto window = sf::RenderWindow{ { windowWidth, windowHeight }, "CMake SFML Project" };
+    window.setVerticalSyncEnabled(true);
+    sf::Image bitmap;
+    bitmap.create(windowWidth, windowHeight, sf::Color::Black);
+    auto texture = sf::Texture();
+
+
     for (int y = 0; y < vrEmuLcdNumPixelsY(lcd); ++y) {
         for (int x = 0; x < vrEmuLcdNumPixelsX(lcd); ++x) {
             // do whatever you like with the pixel information. render it to a texture, output it to  a console, whatever
             // values returned are:  -1 = no pixel (character borders), 0 = pixel off, 1 = pixel on
             char pixel = vrEmuLcdPixelState(lcd, x, y);
             if (pixel == -1)
-                std::cout<<" ";
+                setPixel(bitmap, x,y,noPixel);
             else if (pixel == 0)
-                std::cout << " ";
+                setPixel(bitmap, x,y,clearPixel);
             else if (pixel == 1)
-                std::cout << "*";
+                setPixel(bitmap, x,y,darkPixel);
         }
-        std::cout << '\n';
     }
 
-    std::cout << "done";
-
-
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    texture.loadFromImage(bitmap);
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setOrigin(0,0);
 
     while (window.isOpen())
     {
@@ -63,6 +89,7 @@ int main() {
         }
 
         window.clear();
+        window.draw(sprite);
         window.display();
     }
 
